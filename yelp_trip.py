5da2ac90fb394f57a4891e2dd77bdc9da6b83760
scarolyn <carolyn.tran@outlook.com>
2017-06-12 00:23:18 -0700

    search bar works

--- yelp_trip.py
+++ yelp_trip.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import yelp_api
 app = Flask(__name__)
 
 @app.route('/')
 def yelp_trip():
     return render_template('base.html', api_response = yelp_api.query_api('dinner', 'San Francisco'))
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['search']
+    return render_template('base.html', search_response = query)
 
 if __name__ == '__main__':
     app.run()
