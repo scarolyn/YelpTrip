7ef07d3e67bc90c201c114e58bd7fe29f4229c33
scarolyn <carolyn.tran@outlook.com>
2017-07-09 23:49:30 -0700

    toggle logic

--- static/search.js
+++ static/search.js
@@ -1,1 +1,8 @@
 var clusters = document.getElementById("clusters")
+
+
+// document.getElementById("demo-2").onclick = function() {switch_tab()};
+
+function switch_tab() {
+    document.getElementById("demo-2").classList.add("is-active");
+}
